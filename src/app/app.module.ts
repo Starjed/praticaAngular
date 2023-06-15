6109ff352b83c70009bb34e61de4781ac196c149
Gabriel Albuquerque <biel123albuquerque@hotmail.com>
2023-06-15 17:56:13 -0400

    Adiciona novo projeto no anuglar ja definindo rota principal e nav-bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {SideBarComponent} from './side-bar/side-bar.component';
+import {RegistrosLojasComponent} from './registros-lojas/registros-lojas.component';
+import {IndexComponent} from './index/index.component';
+import {AppRoutingModule} from "./app.routing.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideBarComponent,
+    RegistrosLojasComponent,
+    IndexComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
